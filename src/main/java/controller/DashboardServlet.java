2dd32b95d7a6c50ed4e6cf7ed3fedc6610af24b4
Jimme <duczio12@gmail.com>
2025-11-30 15:33:25 +0700

    Quan ly nhan su kinh doanh BDS

--- src/main/java/controller/DashboardServlet.java
+++ src/main/java/controller/DashboardServlet.java
@@ -1,18 +1,15 @@
 package controller;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/dashboard")
 public class DashboardServlet extends HttpServlet {
+    
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        req.getRequestDispatcher("/admin/dashboard.jsp").forward(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Forward đến trang dashboard
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/dashboard.jsp");
+        dispatcher.forward(request, response);
     }
 }
